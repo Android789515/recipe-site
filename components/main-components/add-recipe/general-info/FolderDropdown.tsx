0d2f8ad9a47ef2f3096486c8504f31c0e8cf1ef4
Android789515 <derflug@mailfence.com>
2021-11-28 15:45:21 -0600

    Adding styling for the first widget in the "add recipe" modal, General info.

--- components/main-components/add-recipe/general-info/FolderDropdown.tsx
+++ components/main-components/add-recipe/general-info/FolderDropdown.tsx
@@ -1,18 +1,20 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Image from '../../../generic-components/Image'
 
+import styles from '../../../../styles/main-components/add-recipe/general-info/FolderDropdown.module.scss'
 import dropdownArrow from '../../../../public/assets/icons/dropdown-arrow.svg'
+
 import useControlledDropdown from '../../../../hooks/useControlledDropdown'
 
 const FolderDropdown = ({  }) => {
-    const { option, chooseOption } = useControlledDropdown()
+    const { option, chooseOption } = useControlledDropdown('Choose or create folder')
 
     // Need to get list of folders
     return (
-        <label>
+        <label className={styles.label}>
             Folder
             <select value={option} required onChange={chooseOption}>
-                <option disabled selected>Choose or create folder</option>
+                <option disabled>Choose or create folder</option>
 
             </select>
 
