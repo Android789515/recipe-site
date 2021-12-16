c83df3210f8747d5471406a4594c3db4d0461323
Android789515 <derflug@mailfence.com>
2021-12-15 20:36:41 -0600

    Working on hero section.

---
+++ components/homepage/hero-section/HeroHeader.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import logo from '../../../public/logo.svg'
+
+import Image from '../../generic-components/Image'
+
+const HeroHeader = () => {
+    return (
+        <header>
+            <Image src={logo} />
+
+            
+        </header>
+    )
+}
+
+export default HeroHeader
