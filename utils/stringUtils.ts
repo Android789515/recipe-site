7583fee6359912c8c28f7422e395f68f741eefdb
Android789515 <derflug@mailfence.com>
2022-01-02 11:27:50 -0600

    Refactored capitalize method for readability.

--- utils/stringUtils.ts
+++ utils/stringUtils.ts
@@ -1,5 +1,6 @@
 export const capitalize = (string: string) => {
     const capitalizedLetter = string.charAt(0).toUpperCase()
+    const restOfWord = string.slice(1)
 
-    return `${capitalizedLetter}${string.slice(1)}`
+    return capitalizedLetter + restOfWord
 }
