e166cdf509d015ee4a84693d2a4abc24dd31b45a
Android789515 <derflug@mailfence.com>
2022-05-22 11:22:39 -0500

    Created some interface functions for accessing and setting user data for the user data context.
    Made the username property optional on the UserData type.

--- hooks/user-auth/userDataContext.tsx
+++ hooks/user-auth/userDataContext.tsx
@@ -1,14 +1,19 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState } from 'react'
 
 import UserData from '../../types/userData'
 
-const userDataContext = createContext<UserData>({})
+const userDataContext = createContext<any>(undefined)
 
 const UserDataProvider: React.FC = ({ children }) => {
 
+    const [ userData, setUserData ] = useState<UserData>()
+
+    const getUsername = () => userData?.username
+
+    const setUser = (userData: UserData) => setUserData(userData)
 
     return (
-        <userDataContext.Provider value={{}}>
+        <userDataContext.Provider value={{ getUsername, setUser }}>
             {children}
         </userDataContext.Provider>
     )
--- types/userData.ts
+++ types/userData.ts
@@ -1,3 +1,5 @@
 export default interface UserData {
+    username?: string
+    email: string
 
 }
