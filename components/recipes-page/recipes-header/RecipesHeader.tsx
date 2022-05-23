b87b5c147f0de9e64bb62e2a509747ab34dcd528
Android789515 <derflug@mailfence.com>
2022-05-22 20:36:32 -0500

    Created SCSS files to style the RecipesHeader components.

--- components/recipes-page/recipes-header/RecipesHeader.tsx
+++ components/recipes-page/recipes-header/RecipesHeader.tsx
@@ -1,10 +1,12 @@
+import styles from '../../../styles/recipes-page/recipes-header/RecipesHeader.module.scss'
+
 import SiteLogo from '../../generic-components/SiteLogo'
 import SearchButton from './search-button/SearchButton'
 import UserWidget from './user-widget/UserWidget'
 
 const RecipesHeader = () => {
     return (
-        <header>
+        <header className={styles.recipesHeader}>
             <SiteLogo />
 
             <SearchButton />
